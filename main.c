04c2e2639c925b57d36cd5aa6f99be1741123255
trio-programming <63399043+trio-programming@users.noreply.github.com>
2020-05-20 12:31:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,10 @@
 
 int main() {
   printf("Bienvenue dans votre console autoresponder.\n        ---         \nCode by trio-programming\nEn attente d'une reponse.\n");
-  char commande [] = "";
+  char commande [70] = "";
+  char reponse [150] = "";
   lire(commande, 70);
+  
   return 0;
 }
+
