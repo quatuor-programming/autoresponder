11f9c60ee867875d4dd19fe0b97e0a9554137a41
trio-programming <63399043+trio-programming@users.noreply.github.com>
2020-05-20 11:47:43 +0200

    Update prototypes.h

--- prototypes.h
+++ prototypes.h
@@ -1,5 +1,5 @@
-#ifndef DEF_PROTOTYPES.H 
-#define DEF_PROTOTYPES.H
+#ifndef DEF_PROTOTYPES
+#define DEF_PROTOTYPES
 
 int lire(char *chaine, int longueur); //lis une chaine de caractere
 void viderBuffer(); //vide le buffer
