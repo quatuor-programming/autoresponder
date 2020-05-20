f2dae4f797f82d1224dc4da6b7da172a47b94b16
trio-programming <63399043+trio-programming@users.noreply.github.com>
2020-05-20 11:36:05 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  printf("Bienvenue dans votre console autoresponder.\n        ---         \nCode by trio-programming\nEm attente d'une reponse.\n");
+  lire
+  return 0;
+}
